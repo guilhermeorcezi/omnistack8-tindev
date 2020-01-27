ed2b4919fb44c377137944b9ff56897f69281eca
Guilherme Orcezi <guilhermeorcezi@gmail.com>
2020-01-26 21:51:45 -0300

    backend almost done

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const mongoose = require('mongoose');
+
+const server = express();
+
+mongoose.connect(
+	'mongodb+srv://guilhermeorcezi:guilhermeorcezi@guilherme-port-gioju.mongodb.net/tindev?retryWrites=true&w=majority',
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	}
+);
+
+server.use(express.json());
+server.use(routes);
+server.use(cors());
+
+server.listen(3333);
